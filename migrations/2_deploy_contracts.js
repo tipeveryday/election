d71a1107bce3d835370d9e8f6bee5a59bb289eee
Kimberly Luu <kim@nuco.io>
2018-12-12 11:56:11 -0500

    Aion Roulette Init Push

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-var Election = artifacts.require("./Election.sol");
+var Casino = artifacts.require("./Casino.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Election);
+  deployer.deploy(web3.toWei(0.1, 'ether'), 100, {gas: 3000000});
 };
