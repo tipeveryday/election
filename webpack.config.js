95cf558a31eed92e7bae958ebf05c47601110a40
John <reelyard@protonmail.com>
2018-12-12 19:08:06 -0500

    Correcting comment helps.

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
    entry: path.join(__dirname, 'src/js', 'index.js'), // Our frontend will be inside the src folder
    output: {
       path: path.join(__dirname, '/'),
-      filename: 'build.js' // The final file will be created in dist/build.js
+      filename: 'build.js' // The final file will be created in /build.js
    },
    module: {
       loaders: [{
